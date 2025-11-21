c6629f8199dc5f3253ac8bcd803d84d38f958f32
hanseul37 <hanseul37@daum.net>
2025-11-22 01:36:49 +0900

    [Style] Log API Summary

--- backend/api/routes/log.py
+++ backend/api/routes/log.py
@@ -7,12 +7,12 @@ from schemas.log import LogResponse, LogCreate
 router = APIRouter()
 
 # 로그 생성
-@router.post("/", response_model=LogResponse)
+@router.post("/", response_model=LogResponse, summary="DB에 새 로그 생성")
 async def create_new_log(log_data: LogCreate, db: Session = Depends(get_db)):
     return create_log(db, log_data)
 
 # 로그 내용 조회
-@router.get("/{deploy_id}", response_model=LogResponse)
+@router.get("/{deploy_id}", response_model=LogResponse, summary="단일 로그 정보 조회")
 async def get_log_by_deploy(deploy_id: int, db: Session = Depends(get_db)):
     log = get_log(db, deploy_id)
 
