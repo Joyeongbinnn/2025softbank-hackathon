493c5a03173872b9c7f518ac9e508f45aa8f0c1b
hanseul37 <hanseul37@daum.net>
2025-11-21 23:59:36 +0900

    [FEAT] Post Log API

--- backend/api/routes/log.py
+++ backend/api/routes/log.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.yoitang import get_db
-from crud.log import get_log
-from schemas.log import LogResponse
+from crud.log import create_log, get_log
+from schemas.log import LogResponse, LogCreate
 
 router = APIRouter()
 
-#  로그 내용 조회
+# 로그 생성
+@router.post("/", response_model=LogResponse)
+async def create_new_log(log_data: LogCreate, db: Session = Depends(get_db)):
+    return create_log(db, log_data)
+
+# 로그 내용 조회
 @router.get("/{deployment_id}", response_model=LogResponse)
 async def get_log_by_deployment(deployment_id: int, db: Session = Depends(get_db)):
     log = get_log(db, deployment_id)
--- backend/crud/log.py
+++ backend/crud/log.py
@@ -1,5 +1,14 @@
 from sqlalchemy.orm import Session
 from models.log import Log
+from schemas.log import LogCreate
+
+# 로그 생성
+def create_log(db: Session, log_data: LogCreate) -> Log:
+    new_log = Log(**log_data.model_dump())
+    db.add(new_log)
+    db.commit()
+    db.refresh(new_log)
+    return new_log
 
 # 로그 정보 조회
 def get_log(db: Session, deployment_id: int) -> Log:
